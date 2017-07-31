356b26c0bc888e64080b369b01abee4dcb107d90
Jennifer Georgevich <jenngeorge@berkeley.edu>
2017-07-31 20:53:11 +0200

    Layout skeleton

---
+++ js/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Scroll from 'react-scroll';
+import Scrollspy from 'react-scrollspy';
+
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+
+const Element = Scroll.Element;
+const Link = Scroll.Link;
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      section: 'about'
+    }
+    this.updateSection = this.updateSection.bind(this);
+  }
+
+  updateSection(newSection){
+    this.setState({section: newSection});
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="nav">
+          <h1 className="logo"> JG </h1>
+          <ul className="nav-list">
+            <Scrollspy items={['about', 'projects', 'contact']} currentClassName='selected' offset={-10}>
+              <Link className='nav-item'
+                onClick={this.updateSection.bind(this, 'about')} to='about' offset={-10} duration={500} smooth={true} delay={50} spy={true}>
+                About
+              </Link>
+              <Link className='nav-item'
+                onClick={this.updateSection.bind(this, 'projects')} to='projects' offset={-10} duration={500} smooth={true} delay={50} spy={true}>
+                Projects
+              </Link>
+              <Link className='nav-item'
+                onClick={this.updateSection.bind(this, 'contact')} to='contact' offset={-10} duration={500} smooth={true} delay={50} spy={true}>
+                Contact
+              </Link>
+            </Scrollspy>
+          </ul>
+        </div>
+
+        <div className='content-wrapper'>
+          <Element id='about' name='about'>
+            <About />
+          </Element>
+          <Element id='projects' name='projects'>
+            <Projects />
+          </Element>
+          <Element id='contact' name='contact'>
+            <Contact />
+          </Element>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './js/resume-site.js',
+  output: {
+    filename: './js/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
